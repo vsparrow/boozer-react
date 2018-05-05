4530a265a5fa2ba8700f42653b1cc25642917856
vsparrow <khan@vsparrow.com>
2018-05-05 02:43:58 -0400

    cleanup comments

--- src/App.js
+++ src/App.js
@@ -13,35 +13,19 @@ class App extends Component {
   }
 
   fetchCocktail = ()=>{                                                         //get details for single cocktail
-    // console.log(this.state.selectedCocktail.id);
     let id = this.state.selectedCocktail.id
     let url = 'http://127.0.0.1:3000/api/v1/cocktails/' + id
-    // console.log(url);
     fetch(url)
     .then(res=>res.json())
-    // .then(json => console.log(json))
-    // .then(json=>this.setProportion(json))
     .then(json => this.setState({proportions: json["proportions"]}))
   }
-  //
-  // setProportion = (obj)=>{
-  //   console.log(obj.proportions);
-  //   // this.setState(proportions:
-  // }
 
   selectCocktail = (event)=>{
-    // console.log(event.target.id);
-    // let cocktail = this.state.cocktails[event.target.id]
     let cocktail = this.state.cocktails.filter((c)=>{return c.name === event.target.id})[0]//[event.target.id]
-    // console.log(cocktail);
     this.setState({selectedCocktail: cocktail},()=>{ this.fetchCocktail()})
   }
 
   render() {
-    // console.log(this.state.cocktails);
-    // console.log(this.state.cocktails.length);
-    // console.log(!!this.state.selectedCocktail);
-    console.log(this.state.proportions);
     return (
       <div className="App">
         <div className="container-fluid">
@@ -60,11 +44,3 @@ class App extends Component {
 }
 
 export default App;
-
-// An Ingredient is a record that indicates,
-//   for example, "Mint Leaves" or "Bitters" is something that many cocktails may have.
-// A Proportion is a join table where each record indicates, a specific cocktail,
-//   is made with a specific ingredient, with a certain amount. For example, a Mojito is made with 4-5 mint leaves.
-
-// Create a component called CocktailsContainer. This component should fetch a list of cocktails from the API
-//   and render out the cocktail names.
--- src/SelectedCocktail.js
+++ src/SelectedCocktail.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 class SelectedCocktail extends Component {
-  // componentDidMount=()=>{
-  //   }
 
   render() {
-    // console.log(this.props.selectedCocktail);
-    // console.log(this.props.proportions);
     let proportions = this.props.proportions.map((p,index)=><li key={index}>{p.amount} {p.ingredient_name}</li>)
-    //amount id ingredient
-    // console.log(this.props.proportions.length);
     return (
       <div className="SelectedCocktail" >
         <h1>{this.props.selectedCocktail.name}</h1>
@@ -28,7 +22,3 @@ class SelectedCocktail extends Component {
 }
 
 export default SelectedCocktail;
-
-// Next, each cocktail name should be a clickable link.
-// Clicking on this link should render out a detail view of the cocktail including
-//   the name, description, instructions, and proportions.
